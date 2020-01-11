c22325829ee0de28abe2b839c8c3044634131f65
bluerat1976 <osoblivaia76@mail.ru>
2020-01-11 20:34:55 +0200

    in process

---
+++ src/HelloWorld.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './HelloWorld.css';
+
+class HelloWorld extends Component {
+    render() {
+        return (
+            <div className='HelloContainer'>
+                <h1>Hello Planet!</h1>
+            </div>
+        );
+    }
+}
+
+export default HelloWorld;
